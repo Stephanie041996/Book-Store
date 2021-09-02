c402513e30aad39ceca3f2a19ab3c02c353e9fd5
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-09-02 15:17:24 +0200

    Implement the App component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import store from './reducers/index';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
