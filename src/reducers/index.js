63277adf4f94206f352c8eb87052e63c99a3935c
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-09-02 15:20:39 +0200

    Add redux store

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers, createStore } from 'redux';
+import bookReducer from './books';
+
+const rootReducer = combineReducers({
+  bookReducer,
+});
+
+const store = createStore(rootReducer);
+export default store;
