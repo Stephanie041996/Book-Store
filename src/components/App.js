60d03365f8414927418019ebb42878e8581a5b42
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-09-06 16:05:17 +0200

    Add nav bar and background color

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import Nav from './nav';
 import './App.css';
 
 const App = () => (
   <div className="App">
+    <Nav />
     <BooksForm />
     <BooksList />
   </div>
---
+++ src/components/nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <div className="panel-bg">
+      <span className="Bookstore-CMS Text-Style-5">
+        Bookstore CMS
+      </span>
+      <span className="BOOKS Text-Style-7">
+        BOOKS
+      </span>
+      <span className="CATEGORIES Text-Style-7">
+        CATEGORIES
+      </span>
+      {/* <div className="Oval">
+        <div className="Mask" />
+      </div> */}
+    </div>
+  );
+}
+export default Nav;
