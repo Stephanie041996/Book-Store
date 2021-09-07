d71e11dce9da9c99af4221c4db447027098d0724
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-09-07 10:13:17 +0200

    Fix repeated code in css

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,13 +5,13 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 function Nav() {
   return (
     <div className="panel-bg">
-      <span className="Bookstore-CMS Text-Style-5">
+      <span className="nav Bookstore-CMS">
         Bookstore CMS
       </span>
-      <span className="BOOKS Text-Style-7">
+      <span className="nav BOOKS ">
         BOOKS
       </span>
-      <span className="CATEGORIES Text-Style-7">
+      <span className=" nav CATEGORIES ">
         CATEGORIES
       </span>
       <div className="user-icon">
